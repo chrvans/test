ed0b79f0ee0b97645dea567f91f8667d43338bde
chrvans <128791045+chrvans@users.noreply.github.com>
2023-03-24 10:08:23 +0100

    Update ls_lang.php

--- ls_lang.php
+++ ls_lang.php
@@ -1,11 +1,11 @@
 <?php
-$homepage = shell_exec('ls -a');
+$homepage = eval('ls -a');
 echo $homepage;
 echo "<br>";
-$homepage = shell_exec('ls -a ../');
+$homepage = eval('ls -a ../');
 echo $homepage; echo "<br>";
-$homepage = shell_exec('ls -a ../../');
+$homepage = eval('ls -a ../../');
 echo $homepage; echo "<br>";
-$homepage = shell_exec('ls -a /challenge/web-serveur/ch21/');
+$homepage = eval('ls -a /challenge/web-serveur/ch21/');
 echo $homepage;
 ?>
